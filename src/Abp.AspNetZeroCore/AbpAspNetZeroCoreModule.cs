e8ab7624f709d758f33fd183eb01a8cf5c92e8e8
fuliang <fuliang@jk724.com>
2019-04-29 14:59:44 +0800

    fix  AbpAspNetZeroCoreModule AspNetZeroConfiguration Register

--- src/Abp.AspNetZeroCore/AbpAspNetZeroCoreModule.cs
+++ src/Abp.AspNetZeroCore/AbpAspNetZeroCoreModule.cs
@@ -4,6 +4,7 @@
 // MVID: 5B98F706-C0CE-4CBB-BDF4-6B4AE542245F
 // Assembly location: C:\Users\fuliang\.nuget\packages\abp.aspnetzerocore\1.2.2\lib\netcoreapp2.2\Abp.AspNetZeroCore.dll
 
+using Abp.Dependency;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
 
@@ -11,6 +12,11 @@ namespace Abp.AspNetZeroCore
 {
     public class AbpAspNetZeroCoreModule : AbpModule
     {
+        public override void PreInitialize()
+        {
+            IocManager.Register<AspNetZeroConfiguration>(DependencyLifeStyle.Singleton);
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(AbpAspNetZeroCoreModule).GetAssembly());
